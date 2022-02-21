f2138d3f3b514fd49594320f4d72444901bbd0d8
slobo10 <97814567+slobo10@users.noreply.github.com>
2022-02-21 14:37:53 -0800

    Add a set interval action to start function

--- MainScript.js
+++ MainScript.js
@@ -1,12 +1,15 @@
 var game = {
     gameArea: 'gameArea',
     index: 0,
+    updateRate: 50,
     start: function () {
         document.getElementById(game.gameArea).addEventListener('keydown', game.keyEventHandeler, false);
+        setInterval(game.mainIterval, 50);
     },
     update: function () {
 
     },
+    mainInterval: function () { game.update() },
     keyEvent: function (e) {
         var key = e.keyCode;
         if (key == 8) {
