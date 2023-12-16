3317680b3a512aec88ebbeeac88fed7b73a31e05
slobo10 <97814567+slobo10@users.noreply.github.com>
2023-12-16 11:04:54 -0800

    Get rid of interval

--- mainScript.js
+++ mainScript.js
@@ -2,21 +2,16 @@ var game = {
   fakeCode: "",
   gameArea: "gameArea",
   index: 0,
-  updateRate: 10,
   start: function () {
     document
       .getElementById(game.gameArea)
       .addEventListener("keydown", game.keyEventHandler, false);
-    setInterval(game.mainInterval, game.updateRate);
   },
   update: function () {
     document.getElementById(game.gameArea).value = game.fakeCode.substring(
       0,
       game.index
     );
-  },
-  mainInterval: function () {
-    game.update();
   },
   keyEvent: function (e) {
     var key = e.keyCode;
@@ -25,6 +20,10 @@ var game = {
     } else if (key != 8 && game.index < game.fakeCode.length) {
       game.index++;
     }
+    //TODO: Find better way to do the following
+    setTimeout(function () {
+      game.update();
+    }, 10);
   },
   keyEventHandler: function (e) {
     game.keyEvent(e);
