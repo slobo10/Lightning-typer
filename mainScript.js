d3f5307d9adfb2096b5de8bd33ef61cc22c02bd9
slobo10 <97814567+slobo10@users.noreply.github.com>
2023-12-16 11:09:13 -0800

    Change vars to lets

--- mainScript.js
+++ mainScript.js
@@ -1,4 +1,4 @@
-var game = {
+let game = {
   fakeCode: "",
   gameArea: "gameArea",
   index: 0,
@@ -14,12 +14,14 @@ var game = {
     );
   },
   keyEvent: function (e) {
-    var key = e.keyCode;
+    let key = e.keyCode;
+
     if (key == 8 && game.index > 0) {
       game.index--;
     } else if (key != 8 && game.index < game.fakeCode.length) {
       game.index++;
     }
+
     //TODO: Find better way to do the following
     setTimeout(function () {
       game.update();
